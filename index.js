e465b714f5395b65e6dc327456761146cec8b7ee
Md Shahazad Uddin <shanudjn@gmail.com>
2021-05-21 01:05:15 +0530

    added middleware

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-const express = require('express')
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
+const { initializeDBConnection } = require('./db/db.connect');
+const { errorHandler } = require('./middleware/errorHandler');
+const { routeNotFound } = require('./middleware/routeNotFound');
+const { populateVideosCollection } = require('./utils/utils');
+
+const videos = require('./routes/video.router')
 
 const app = express();
+app.use(cors());
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json());
+
+
+
+
+initializeDBConnection();
+
+
+
+
 
 app.get('/', (req, res) => {
     res.status(200).json({ "success": true, "message": "Hello Express App" })
 })
 
+app.use('/videos', videos)
+
+// populateVideosCollection();
+app.use(errorHandler);
+app.use(routeNotFound)
 
 app.listen(process.env.PORT || 8080, () => {
     console.log('Server Started ✅')
